2bc806739e2e4da112a2b4013c6e059e3a1059c7
coderamen <spencerteh@gmail.com>
2017-02-17 17:08:32 +0800

    copy/paste bitly code from previous project

--- app/models/url.rb
+++ app/models/url.rb
@@ -1,2 +1,9 @@
 class Url < ApplicationRecord
+    require 'securerandom'
+    validates :long_url, presence: { message: 'URL required' }
+    validates :long_url, format: { with: URI.regexp }, uniqueness: true
+
+    def shorten
+        short_url = SecureRandom.hex(3)
+    end
 end
