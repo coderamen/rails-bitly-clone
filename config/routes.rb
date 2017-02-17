c4b1276926a9c77f9a4fe7ff1b26c228753dba8b
coderamen <spencerteh@gmail.com>
2017-02-17 19:21:03 +0800

    Exercise.135/done

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  resource :urls
-  root 'urls#index'
+    # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+    resources :urls
+    root 'urls#index'
 end
